d9bb2d622ca7ae5b63431d3258651d9ff931bb68
JuanSolerC <jsolercastellanos@gmail.com>
2023-12-12 18:33:55 -0500

    update2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-};
 
-module.exports = nextConfig;
+const nextConfig = {
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+      };
+    },
+
+  };
+  
+  module.exports = nextConfig;
+  
